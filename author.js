8c39eb9eb13096ca438a69e598f7c87ba7f724cc
Jeremy Poulter <Jpoulter@gmail.com>
2020-01-04 22:40:46 -0700

    pass book.js tests and first few author.js tests

--- author.js
+++ author.js
@@ -1,5 +1,12 @@
 class Author {
+  constructor(authorObj) {
+    this.first = authorObj.first;
+    this.last = authorObj.last;
+    this.books = [];
+  }
+  write(bookTitle, bookPub) {
 
+  }
 }
 
 module.exports = Author;
--- book.js
+++ book.js
@@ -1,5 +1,9 @@
 class Book {
-
+  constructor(first, last, title, publicationYear) {
+    this.author = first + ' ' + last;
+    this.title = title;
+    this.publicationYear = publicationYear;
+  }
 }
 
 module.exports = Book;

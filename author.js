f2304f01d3bb211c558649593625e74ea18a59bf
Jeremy Poulter <Jpoulter@gmail.com>
2020-01-05 14:52:46 -0700

    pass remaining tests on author

--- author.js
+++ author.js
@@ -1,11 +1,22 @@
+var Book = require('./book.js');
+
 class Author {
   constructor(authorObj) {
     this.first = authorObj.first;
     this.last = authorObj.last;
     this.books = [];
   }
+  
   write(bookTitle, bookPub) {
+    var newBook = new Book(
+      this.first,
+      this.last,
+      bookTitle,
+      bookPub
+    );
 
+    this.books.push(newBook);
+    return newBook;
   }
 }
 
